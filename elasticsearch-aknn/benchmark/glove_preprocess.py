a351087c86785c07d27c2fcfd8f177f4c6b688ce
mingruimingrui <mingrui.wang@shopee.com>
2018-09-22 11:12:51 +0800

    removed trailing white space

--- elasticsearch-aknn/benchmark/glove_preprocess.py
+++ elasticsearch-aknn/benchmark/glove_preprocess.py
@@ -5,7 +5,7 @@ assert len(sys.argv) > 1, "Usage: <script.py> path-to-unzipped-glove-vecs.txt"
 
 for i, line in enumerate(open(sys.argv[1])):
     tkns = line.split(" ")
-    word = tkns[0] 
+    word = tkns[0]
     vector = list(map(lambda x: round(float(x), 5), tkns[1:]))
     doc = {
         "_id": "word_%d" % i,
