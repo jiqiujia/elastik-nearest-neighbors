614e82e381a9521a450175005be2d75875218331
mingruimingrui <mingrui.wang@shopee.com>
2018-09-22 11:55:25 +0800

    changed version to 5.6.5

--- elasticsearch-aknn/testplugin.sh
+++ elasticsearch-aknn/testplugin.sh
@@ -2,16 +2,14 @@
 # Script for quickly recompiling and testing the elasticsearch-aknn plugin.
 set -e
 
-ESBIN="$HOME/Downloads/elasticsearch-6.2.4/bin"
-PLUGINPATH="file:build/distributions/elasticsearch-aknn-0.0.1-SNAPSHOT.zip"
+ESBIN="$HOME/elastic-stack/elasticsearch-5.6.5/bin"
+PLUGINPATH="file:`pwd`/build/distributions/elasticsearch-aknn-0.0.1-SNAPSHOT.zip"
 
 # TODO: fix the code so that skipping these tasks is not necessary.
-gradle clean build -x integTestRunner -x test 
+gradle clean build -x integTestRunner -x test
 $ESBIN/elasticsearch-plugin remove elasticsearch-aknn | true
 $ESBIN/elasticsearch-plugin install -b $PLUGINPATH
 
 sudo sysctl -w vm.max_map_count=262144
-export ES_HEAP_SIZE=12g 
+export ES_HEAP_SIZE=12g
 $ESBIN/elasticsearch
-
-
